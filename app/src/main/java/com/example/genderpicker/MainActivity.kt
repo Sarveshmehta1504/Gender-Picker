b448098f226cf4deaac7dbb81deca987832b2ac3
Sarvesh Mehta <sarveshmehta1504@gmail.com>
2024-06-22 23:50:02 +0530

    Initial commit

---
+++ app/src/main/java/com/example/genderpicker/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.genderpicker
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import com.example.genderpicker.ui.theme.GenderPickerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            GenderPickerTheme {
+                GenderPicker (
+                    modifier = Modifier.fillMaxSize()
+                ){}
+            }
+        }
+    }
+}
